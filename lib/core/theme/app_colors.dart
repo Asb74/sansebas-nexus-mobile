99b32ce900dbc18a935bdfa4e042db93238d7070
Antonio Sánchez Buzón <amtoniosazon@gmail.com>
2026-07-02 00:40:21 +0200

    Fix deleted sync status color

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -18,6 +18,7 @@ class AppColors {
   static const uploading = Color(0xFF3B82F6);
   static const uploaded = Color(0xFF10B981);
   static const imported = Color(0xFF6366F1);
+  static const deleted = Color(0xFF94A3B8);
   static const error = Color(0xFFEF4444);
 
   static Color forSyncStatus(SyncStatus status) {
@@ -26,6 +27,7 @@ class AppColors {
       SyncStatus.uploading => uploading,
       SyncStatus.uploaded => uploaded,
       SyncStatus.imported => imported,
+      SyncStatus.deleted => deleted,
       SyncStatus.error => error,
     };
   }
